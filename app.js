0a77f5d42ed27fe09f541e6a6b20e5b94fd126d3
newstar <kangsaebyeol@gmail.com>
2022-04-26 16:09:27 +0900

    Add variables for board size management

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const gameBoard = document.getElementById('game-board')
+const boardSize = 20
 const startBtn = document.querySelector('.start')
 const snakeBody = [{ x: 1, y: 1 }]
 const snakeElement = document.createElement('div')
+
+setBoard(boardSize)
+
+function setBoard(boardSize) {
+  boardSize = `repeat(${boardSize},1fr)`
+  gameBoard.style.gridTemplateRows = boardSize
+  gameBoard.style.gridTemplateColumns = boardSize
+}
 
 function draw(gameBoard) {
   snakeBody.forEach(segment => {
@@ -17,22 +26,26 @@ function control(e) {
   if (e.keyCode === 39) {
     // right
     snakeBody.forEach(segment => {
-      snakeElement.style.gridColumnStart = segment.x++ + 1
+      console.log(`${segment.x} / ${segment.y}`)
+      snakeElement.style.gridColumnStart = ++segment.x
     })
   } else if (e.keyCode === 38) {
     // top
     snakeBody.forEach(segment => {
-      snakeElement.style.gridRowStart = segment.y-- - 1
+      console.log(`${segment.x} / ${segment.y}`)
+      snakeElement.style.gridRowStart = --segment.y
     })
   } else if (e.keyCode === 37) {
     // left
     snakeBody.forEach(segment => {
-      snakeElement.style.gridColumnStart = segment.x-- - 1
+      console.log(`${segment.x} / ${segment.y}`)
+      snakeElement.style.gridColumnStart = --segment.x
     })
   } else if (e.keyCode === 40) {
     // bottom
     snakeBody.forEach(segment => {
-      snakeElement.style.gridRowStart = segment.y++ + 1
+      console.log(`${segment.x} / ${segment.y}`)
+      snakeElement.style.gridRowStart = ++segment.y
     })
   }
 }
